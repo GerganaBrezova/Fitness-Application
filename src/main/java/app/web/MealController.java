3ebc66e665cb9b8c950957a6f40eabd10f8f6672
Gergana Brezova <dimitrovabrezova@gmail.com>
2025-04-03 00:43:34 +0300

    Removed import.

--- src/main/java/app/web/MealController.java
+++ src/main/java/app/web/MealController.java
@@ -13,7 +13,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 import java.util.UUID;
 
 @Controller
