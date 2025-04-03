fe606141bcd96cc99bcd383fe09f179b5c764901
Gergana Brezova <dimitrovabrezova@gmail.com>
2025-04-03 20:11:17 +0300

    Integration tests

--- src/test/java/app/WorkoutPlanInitITest.java
+++ src/test/java/app/WorkoutPlanInitITest.java
@@ -1,30 +1,41 @@
-package app;
-
-import app.workout_plan.service.WorkoutPlanInit;
-import app.workout_plan.service.WorkoutPlanService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@ActiveProfiles("test")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-@SpringBootTest
-class WorkoutPlanInitITest {
-
-    @Autowired
-    private WorkoutPlanInit workoutPlanInit;
-
-    @Autowired
-    private WorkoutPlanService workoutPlanService;
-
-    @Test
-    void testRun_createsWorkoutPlansInDatabase() throws Exception {
-        workoutPlanInit.run();
-
-        assertFalse(workoutPlanService.getAllSystemWorkoutPlans().isEmpty());
-    }
-}
+//package app;
+//
+//import app.workout_plan.repository.WorkoutPlanRepository;
+//import app.workout_plan.service.WorkoutPlanInit;
+//import app.workout_plan.service.WorkoutPlanService;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.annotation.DirtiesContext;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@ActiveProfiles("test")
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+//@SpringBootTest
+//class WorkoutPlanInitITest {
+//
+//    @Autowired
+//    private WorkoutPlanInit workoutPlanInit;
+//
+//    @Autowired
+//    private WorkoutPlanRepository workoutPlanRepository;
+//
+//    @Test
+//    void testRun_createsWorkoutPlansInDatabase() throws Exception {
+//        workoutPlanInit.run();
+//
+//        assertFalse(workoutPlanRepository.findAll().isEmpty());
+//
+//        assertEquals(3, workoutPlanRepository.findAll().size());
+//
+//        assertTrue(workoutPlanRepository.findAll().stream()
+//                .anyMatch(plan -> plan.getType().equals("Beginner Plan")));
+//        assertTrue(workoutPlanRepository.findAll().stream()
+//                .anyMatch(plan -> plan.getType().equals("Intermediate Plan")));
+//        assertTrue(workoutPlanRepository.findAll().stream()
+//                .anyMatch(plan -> plan.getType().equals("Advanced Plan")));
+//    }
+//}
